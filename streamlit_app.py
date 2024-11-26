d634b6981fd04780f1c6732a7649f90e69993ac6
snowflakeclass <malloryhu@yahoo.com>
2024-11-25 21:17:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,10 @@ if ingredients_list:
     #st.stop()
     time_to_insert = st.button('Submit Order')
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
